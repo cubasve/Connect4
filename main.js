ea3c750d81f7545e0df580165a4ea06b940ec231
cubasve <eva.cubasvasquez@gmail.com>
2021-02-17 20:03:30 -0500

    Add render function

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ let board, winner, turn;
 /*----------CACHED ELEMENTS--------*/
 const messageEl = document.getElementById('message');
 const markersEl = document.getElementById('markers');
+let cellEls;
 
 /*----------EVENTS-------------*/
 
@@ -19,7 +20,30 @@ const markersEl = document.getElementById('markers');
 initialize();
 
 function render() {
-    console.log('render');
+    board.forEach((columnArray, columnIndex) => {
+        const markerEl = document.getElementById(`column${columnIndex}`);
+        //If there is a 0 left in the column, we want the background color to be grey
+        //If there are no 0's left in the column (full), want it to be transparent
+        markerEl.style.borderTopColor = columnArray.includes(0) ? 'grey' : 'transparent';
+
+        //cell = each circle in the board --> value is 1, 0, or -1
+        columnArray.forEach((cell, rowIndex) => {
+            //Ex. color[cell] = color[1] (object[key])= blue (value)
+            cellEls[columnIndex][rowIndex].style.backgroundColor = colors[cell];
+        });
+    });
+
+    if (winner) {
+        if (winner === 'T') {
+            messageEl.textContent = 'It\'s a tie!';
+        } else {
+            let color = colors[winner];
+            messageEl.innerHTML = `<span style="color:${color}">${color.toUpperCase()}</span> wins!`;
+        }
+    } else {
+        let color = colors[turn];
+        messageEl.innerHTML = `<span style="color:${color}">${color.toUpperCase()}'s</span> turn`;
+    }
 }
 
 function initialize() {
@@ -34,7 +58,18 @@ function initialize() {
         [0, 0, 0, 0, 0, 0], //column5
         [0, 0, 0, 0, 0, 0], //column6
     ];
+
+    cellEls = [];
+    for (let columnIndex = 0; columnIndex < board.length; columnIndex++) {
+        let temp = [];
+        for (let rowIndex = 0; rowIndex < board[columnIndex].length; rowIndex++) {
+            let el = document.getElementById(`column${columnIndex}row${rowIndex}`);
+            temp.push(el);
+        }
+        cellEls.push(temp);
+    }
+
     winner = null;
     turn = 1; //Player 1(blue)'s turn is first
-    // render();
+    render();
 }
